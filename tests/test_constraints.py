40938a89382522cc528d7b6c26f0322d4260e42d
Mattias.Poppe <poppe.mattias@gmail.com>
2025-07-02 11:26:46 +0200

    Add MeemooSIPConstraintEvaluation test

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -2,6 +2,8 @@ from meemoo_sip_validator.constraints import (
     MeemooSIPConstraint,
     MeemooSIPConstraintCardinality,
     MeemooSIPConstraintDatatype,
+    MeemooSIPConstraintEvaluation,
+    MeemooSIPConstraintEvaluationStatus,
     MeemooSIPConstraintObligation,
     MeemooSIPConstraintSIPLevel,
     MeemooSIPConstraintXMLNodeType,
@@ -25,3 +27,16 @@ class TestMeemooSIPConstraint:
         assert constraint.xml_node_type == MeemooSIPConstraintXMLNodeType.UNSPECIFIED
         assert constraint.filename is None
         assert constraint.xpath is None
+
+
+class TestMeemooSIPConstraintEvaluation:
+    def test_init(self):
+        identification = "id01"
+        description = "description"
+        constraint = MeemooSIPConstraint(identification, description)
+        evaluation_status = MeemooSIPConstraintEvaluationStatus.SUCCESS
+        evaluation = MeemooSIPConstraintEvaluation(constraint, evaluation_status)
+
+        assert evaluation.constraint == constraint
+        assert evaluation.status == evaluation_status
+        assert evaluation.message is None
