c598da3b197aadb120b01b00e00fb28dddbc85ab
Luca Soto De Graeve <lucasotodegraeve@gmail.com>
2025-07-24 01:48:16 +0200

    Add examples as submodule and use for tests

---
+++ tests/v2_1/test_examples.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pytest
+
+from meemoo_sip_validator.v2_1 import validate_to_report
+
+sip_paths = list(Path("tests/sip-examples/2.1").iterdir())
+
+# TODO: This sip has not been migrated fully to 2.1
+sip_paths = [p for p in sip_paths if "ftp_sidecar" not in str(p)]
+
+unzipped_paths = [next(path.iterdir()) for path in sip_paths]
+
+
+@pytest.mark.parametrize("unzipped_path", unzipped_paths)
+def test_examples(unzipped_path: Path):
+    report = validate_to_report(unzipped_path)
+    assert report.outcome == "PASSED"
