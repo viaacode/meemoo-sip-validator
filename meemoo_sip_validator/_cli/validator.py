e36f22f5da8447c9e64ac0abaf42e0a9c8292a0a
Luca Soto De Graeve <lucasotodegraeve@gmail.com>
2025-09-25 16:15:53 +0200

    Specify version in pyproject and update readme

--- meemoo_sip_validator/_cli/validator.py
+++ meemoo_sip_validator/_cli/validator.py
@@ -1,29 +1,38 @@
 import sys
 from pathlib import Path
 import json
+from importlib.metadata import version
 
 
 def validator_cli():
+    if len(sys.argv) == 2 and sys.argv[1] == "--version":
+        print(f"meemoo-sip-validator {version('meemoo-sip-validator')}")
+        exit()
+
     if len(sys.argv) != 3:
-        print("Usage: meemoo-sip-validator VERSION PATH\n\nSupported SIP versions: 2.1")
+        print(
+            "Usage: meemoo-sip-validator SIP-VERSION PATH\n\nSupported SIP versions: 2.1"
+        )
         exit(1)
 
-    version = sys.argv[1]
+    version_arg = sys.argv[1]
     path = Path(sys.argv[2])
-    validator_fn = get_validator_for_version(version)
+    validator_fn = get_validator_fn_for_version(version_arg)
     report = validator_fn(path)
     failures = [failure.to_dict() for failure in report.failures]
 
     print(json.dumps(failures, indent=4))
     if report.is_valid:
-        print("\nSIP is valid.")
+        print(
+            "\nNo error produced. The SIP may still be invalid, as the validator is incomplete."
+        )
         exit(0)
     else:
         print("\nSIP is not valid.")
         exit(1)
 
 
-def get_validator_for_version(version: str):
+def get_validator_fn_for_version(version: str):
     match version:
         case "2.1":
             from ..v2_1 import validate_to_report
